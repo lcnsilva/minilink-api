e89c6423869d78b88098099e748e2fc4d277ac43
Luciano Silva <lcnsilvajf@gmail.com>
2025-05-14 20:30:06 -0300

    fix: remove trailing slash from production URL in allowedOrigins

--- src/index.ts
+++ src/index.ts
@@ -28,7 +28,7 @@ const app = express();
 const allowedOrigins : string[] = [
     'http://localhost:5173',
     'http://localhost:3000',
-    'https://minilink-one.vercel.app/'
+    'https://minilink-one.vercel.app'
 ]
 
 const corsOptions: cors.CorsOptions = {
